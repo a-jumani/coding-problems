c48f6d1a22df42cf1514b36bcbe1082949b3ee3c
a-jumani <arsalan.jumani@outlook.com>
2019-12-06 14:49:17 +0400

    feat: Skeleton for painter's partition problem

---
+++ Binary Search/2/painters_partition.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+// solve painter's partition problem using binary search with DP
+// complexity: (time, space) = (O(k n log n), O(n))
+// preconditions: *prefix_sum has n elements; n > 0; k > 0
+int partition_dp(const vector<int> prefix_sum, const int n, const int k) {
+    return 0;
+}
+
+// solve painter's partition problem using binary search
+// complexity: (time, space) = (O(n log n), O(1))
+// preconditions: *prefix_sum has n elements; n > 0; k > 0
+int partition_bs(const vector<int> prefix_sum, const int n, const int k) {
+    return 0;
+}
+
+// brute-force solution - ref: geeksforgeeks.org/painters-partition-problem
+// preconditions: *prefix_sum has n elements; n > 0; k > 0
+int partition_bf(const vector<int> prefix_sum, const int n, const int k) {
+    return 0;
+}
