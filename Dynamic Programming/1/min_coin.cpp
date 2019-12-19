fe42cc8c3581219dca0b3d59c913229599e9a39d
a-jumani <arsalan.jumani@outlook.com>
2019-12-19 20:43:25 +0400

    feat: Add skeleton for min coin change dp

---
+++ Dynamic Programming/1/min_coin.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+// minimum number of coins needed for change of given
+// amount; -1 if change is not possible
+//
+// preconditions:
+//   coins[i] >= 0
+//   amount >= 0
+int coin_change(const vector<int>& coins, const int amount) {
+    return -1;
+}
---
+++ Dynamic Programming/README.md
@@ -0,0 +1,4 @@
+Interesting dynamic programming problems, optimizations or formulations.
+
+**Problems:**
+1. Minimum coins for change ([LeetCode](https://leetcode.com/problems/coin-change/)): (time, space) = (O(nM), O(nM)), where n is the number of coins and M is the amount
