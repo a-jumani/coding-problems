755bdfb1953da5c22648ac40791ebe9d79e8ba2c
a-jumani <arsalan.jumani@outlook.com>
2019-12-22 13:35:40 +0400

    feat: Add skeleton for 0-1 knapsack

---
+++ Dynamic Programming/2/knapsack.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+// max value of items that can be fit in capacity C
+//
+// preconditions:
+//   w[i] >= 0
+//   C >= 0
+int knapsack(const vector<int> &v, const vector<int> &w, int C) {
+    return -1;
+}
