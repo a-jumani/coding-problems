bc7ca087758620941f976343f992e4e31771aeef
a-jumani <arsalan.jumani@outlook.com>
2019-12-22 13:49:12 +0400

    feat: Add 0-1 knapsack impl using memo swap

--- Dynamic Programming/2/knapsack.cpp
+++ Dynamic Programming/2/knapsack.cpp
@@ -4,9 +4,30 @@ using namespace std;
 
 // max value of items that can be fit in capacity C
 //
+// eq: S(c, i) = max{v[i] + S(c-w[i], i-1), S(c, i-1)},
+// where S(c, i) is the maximum value with capacity c
+// items[:i+1].
+//
 // preconditions:
 //   w[i] >= 0
 //   C >= 0
 int knapsack(const vector<int> &v, const vector<int> &w, int C) {
-    return -1;
+    // initialize memo for 0 items
+    vector<int> memo_old(C+1, 0), memo_new(C+1, 0);
+
+    for ( int i = 0; i < v.size(); ++i ) {
+        for ( int c = 0; c <= C; ++c ) {
+
+            // don't include item i
+            memo_new[c] = memo_old[c];
+
+            // include item i
+            if ( w[i] <= c )
+                memo_new[c] = max(memo_new[c], memo_old[c-w[i]] + v[i]);
+        }
+
+        swap(memo_old, memo_new);
+    }
+
+    return memo_old.back();
 }
--- Dynamic Programming/README.md
+++ Dynamic Programming/README.md
@@ -2,3 +2,4 @@ Interesting dynamic programming problems, optimizations or formulations.
 
 **Problems:**
 1. Minimum coins for change ([LeetCode](https://leetcode.com/problems/coin-change/)): (time, space) = (O(nM), O(M)), where n is the number of coins and M is the amount
+2. 0-1 knapsack ([GeeksforGeeks](https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/)): (time, space) = (O(nC), O(C)), where n is the number of items and C is the capacity
