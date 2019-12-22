96fd4e27e9e14c2bd525a9b7019f5da8ef405338
a-jumani <arsalan.jumani@outlook.com>
2019-12-22 14:04:20 +0400

    feat: Optimize space constants for knapsack

--- Dynamic Programming/2/knapsack.cpp
+++ Dynamic Programming/2/knapsack.cpp
@@ -13,21 +13,12 @@ using namespace std;
 //   C >= 0
 int knapsack(const vector<int> &v, const vector<int> &w, int C) {
     // initialize memo for 0 items
-    vector<int> memo_old(C+1, 0), memo_new(C+1, 0);
+    vector<int> memo(C+1, 0);
 
-    for ( int i = 0; i < v.size(); ++i ) {
-        for ( int c = 0; c <= C; ++c ) {
-
-            // don't include item i
-            memo_new[c] = memo_old[c];
-
-            // include item i
+    for ( int i = 0; i < v.size(); ++i )
+        for ( int c = C; c >= 0; --c )
             if ( w[i] <= c )
-                memo_new[c] = max(memo_new[c], memo_old[c-w[i]] + v[i]);
-        }
+                memo[c] = max(memo[c], memo[c-w[i]] + v[i]);
 
-        swap(memo_old, memo_new);
-    }
-
-    return memo_old.back();
+    return memo.back();
 }
