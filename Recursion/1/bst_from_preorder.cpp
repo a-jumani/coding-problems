351d4210c3b2681c5d4b4c8bb55ac18209933b3b
a-jumani <arsalan.jumani@outlook.com>
2019-12-25 14:17:20 +0400

    feat: Sol for BST from preorder

--- Recursion/1/bst_from_preorder.cpp
+++ Recursion/1/bst_from_preorder.cpp
@@ -1,3 +1,5 @@
+#include <climits>
+#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -9,6 +11,32 @@ struct tree_node {
     tree_node(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
+// recover binary search tree r rooted at preorder[i]
+// 
+// preconditions:
+//   stop >= node.val if node in r
+tree_node *bst_from_preorder_rec(const vector<int> &preorder, int &i, int stop = INT_MAX) {
+    
+    // base case
+    if ( i == preorder.size() || preorder[i] > stop )
+        return nullptr;
+    
+    // first value is the root's in pre-order traversal
+    tree_node *root = new tree_node(preorder[i++]);
+
+    // left subtree has values less than root->val
+    root->left = bst_from_preorder_rec(preorder, i, root->val);
+
+    // right subtree has values less than root's parent node
+    root->right = bst_from_preorder_rec(preorder, i, stop);
+
+}
+
+// recover binary search tree from the given pre-order traversal
+//
+// preconditions:
+//   preorder contains no duplicates
 tree_node *bst_from_preorder(const vector<int> &preorder) {
-    // code
+    int i = 0;
+    return bst_from_preorder_rec(preorder, i);
 }
