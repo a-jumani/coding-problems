bbcba69bf7690a3e1f36a6f1ff793db51c201137
a-jumani <arsalan.jumani@outlook.com>
2019-12-28 22:03:12 +0400

    feat: Sol for dll to BST conversion

--- Recursion/2/dll_to_bst.cpp
+++ Recursion/2/dll_to_bst.cpp
@@ -14,5 +14,21 @@ struct node {
 //   head_ref != nullptr
 // returns root node of the BST
 node *dll_to_bst(node **head_ref, int n) {
-    // code
+
+    // base case - no list node to process
+    if ( n == 0 )
+        return nullptr;
+    
+    // get left subtree
+    node *left = dll_to_bst(head_ref, n/2);
+    root->prev = left;
+
+    // consume current node as root
+    node *root = *head_ref;
+    *head_ref = root->next;
+
+    // get right subtree
+    root->next = dll_to_bst(head_ref, (n+1)/2 - 1);
+
+    return root;
 }
